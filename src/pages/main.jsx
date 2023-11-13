e71af392f50a3067027105f90a91c9cbd25dbb01
Lucas Ricci <lucasrafael.ricci@gmail.com>
2023-11-12 22:23:22 -0300

    Cria tela de login do rocket notes

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
+import { SignIn } from './SignIn';
 import { Details } from './Details';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
